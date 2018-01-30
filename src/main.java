1c9771f2a79e48db1de1d2a38006b4f7afade350
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-01-30 13:44:20 +0100

    Done

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+
+        // Hej Troels Dette er den første opgave, det vildt
+        for (int i = 1; i < 11  ; i++) {
+            System.out.println(i);
+        }
+    }
+}
